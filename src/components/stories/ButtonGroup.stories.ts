d56ba33c22e8e734863c8cd7a3acbc419228c6e6
Edward Heaver <edward.heaver01@gmail.com>
2025-07-22 17:40:45 +0100

    Some changes to the responsive wrapper. I know how we're going to do decorators for astro and maybe svelte now

--- src/components/stories/ButtonGroup.stories.ts
+++ src/components/stories/ButtonGroup.stories.ts
@@ -8,6 +8,8 @@ export default {
 
 export const Regular = {
     args: {
+        simulatedHeight: "600px",
+        simulatedWidth: "1200px",
         componentRoute: "/ui/UIButtonGroup"
     }
 }
@@ -15,6 +17,7 @@ export const Regular = {
 export const SmallScreen = {
     args: {
         simulatedWidth: "400px",
+        simulatedHeight: "800px",
         componentRoute: "/ui/UIButtonGroup"
     }
 }
